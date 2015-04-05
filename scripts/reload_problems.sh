e0ca8f7fff796c0221a3fba34dd1b51d7fd182f6
Danny Qiu <dqiu55@gmail.com>
2015-04-05 01:41:53 -0400

    Modify scripts

--- scripts/reload_problems.sh
+++ scripts/reload_problems.sh
@@ -1,3 +1,10 @@
+# Makes sure that we are in the correct directory to work in
 cd /home/vagrant/api
+
+echo "Clearing database of problems"
 python3 api_manager.py database clear problems
+
+echo "Reloading problems"
 python3 api_manager.py -v problems load /vagrant/problems/ graders/ ../problem_static/
+
+devploy
